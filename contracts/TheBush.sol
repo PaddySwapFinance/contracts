c9daa05bf62481fd90ba674bce25d10bfed1327c
Mohamed Doffer Mohamed Nasir <73598690+mohamed-nasir@users.noreply.github.com>
2021-10-08 10:28:41 +0800

    v1.0.0

---
+++ contracts/TheBush.sol
@@ -0,0 +1,187 @@
+pragma solidity 0.6.12;
+
+// based on PanCakeSwap SmartChef contract
+
+import "./SafeMath.sol";
+import "./Ownable.sol";
+import "./IBEP20.sol";
+import "./SafeBEP20.sol";
+
+contract TheBush is Ownable {
+    using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IBEP20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. SROCKs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that SROCKs distribution occurs.
+        uint256 accSrockPerShare; // Accumulated SROCKs per share, times 1e12. See below.
+    }
+
+    // The SROCK TOKEN!
+    IBEP20 public srock;
+    IBEP20 public rewardToken;
+
+    // SROCK tokens created per block.
+    uint256 public rewardPerBlock;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping (address => UserInfo) public userInfo;
+    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    uint256 private totalAllocPoint = 0;
+    // The block number when SROCK mining starts.
+    uint256 public startBlock;
+    // The block number when SROCK mining ends.
+    uint256 public bonusEndBlock;
+
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 amount);
+
+    constructor(
+        IBEP20 _srock,
+        IBEP20 _rewardToken,
+        uint256 _rewardPerBlock,
+        uint256 _startBlock,
+        uint256 _bonusEndBlock
+    ) public {
+        srock = _srock;
+        rewardToken = _rewardToken;
+        rewardPerBlock = _rewardPerBlock;
+        startBlock = _startBlock;
+        bonusEndBlock = _bonusEndBlock;
+
+        // staking pool
+        poolInfo.push(PoolInfo({
+            lpToken: _srock,
+            allocPoint: 1000,
+            lastRewardBlock: startBlock,
+            accSrockPerShare: 0
+        }));
+
+        totalAllocPoint = 1000;
+
+    }
+
+    function stopReward() public onlyOwner {
+        bonusEndBlock = block.number;
+    }
+
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        if (_to <= bonusEndBlock) {
+            return _to.sub(_from);
+        } else if (_from >= bonusEndBlock) {
+            return 0;
+        } else {
+            return bonusEndBlock.sub(_from);
+        }
+    }
+
+    // View function to see pending Reward on frontend.
+    function pendingReward(address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[0];
+        UserInfo storage user = userInfo[_user];
+        uint256 accSrockPerShare = pool.accSrockPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 srockReward = multiplier.mul(rewardPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accSrockPerShare = accSrockPerShare.add(srockReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accSrockPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 srockReward = multiplier.mul(rewardPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        pool.accSrockPerShare = pool.accSrockPerShare.add(srockReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+
+    // Stake SROCK tokens to SmartChef
+    function deposit(uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[0];
+        UserInfo storage user = userInfo[msg.sender];
+
+        updatePool(0);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accSrockPerShare).div(1e12).sub(user.rewardDebt);
+            if(pending > 0) {
+                rewardToken.safeTransfer(address(msg.sender), pending);
+            }
+        }
+        if(_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accSrockPerShare).div(1e12);
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    // Withdraw SROCK tokens from STAKING.
+    function withdraw(uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[0];
+        UserInfo storage user = userInfo[msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(0);
+        uint256 pending = user.amount.mul(pool.accSrockPerShare).div(1e12).sub(user.rewardDebt);
+        if(pending > 0) {
+            rewardToken.safeTransfer(address(msg.sender), pending);
+        }
+        if(_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accSrockPerShare).div(1e12);
+
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw() public {
+        PoolInfo storage pool = poolInfo[0];
+        UserInfo storage user = userInfo[msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+        emit EmergencyWithdraw(msg.sender, user.amount);
+    }
+
+    // Withdraw reward. EMERGENCY ONLY.
+    function emergencyRewardWithdraw(uint256 _amount) public onlyOwner {
+        require(_amount < rewardToken.balanceOf(address(this)), 'not enough token');
+        rewardToken.safeTransfer(address(msg.sender), _amount);
+    }
+
+}
